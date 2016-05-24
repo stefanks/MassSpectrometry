8f313a881b0f7423d6d8fcd5d9db5b11c4bfcb15
stefanks <solntsev@gmail.com>
2016-05-24 11:16:10 -0500

    Added tests

--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MassSpectrometry")]
-[assembly: AssemblyDescription("Library for Mass Spectrometry objects")]
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Stefan Solntsev")]
-[assembly: AssemblyProduct("MassSpectrometry")]
-[assembly: AssemblyCopyright("Copyright 2016 Stefan Solntsev")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9bf1f2fd-c17a-4151-861a-939fe342ec38")]
+[assembly: Guid("36e87f6e-1ec4-4496-b50d-7fd5a82b6177")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test/TestSpectra.cs
@@ -0,0 +1,214 @@
+﻿// Copyright 2012, 2013, 2014 Derek J. Bailey
+//
+// This file (SpectrumTestFixture.cs) is part of CSMSL.Tests.
+//
+// CSMSL.Tests is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CSMSL.Tests is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with CSMSL.Tests. If not, see <http://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using Spectra;
+
+namespace Test
+{
+    [TestFixture, Category("Spectral")]
+    public sealed class SpectrumTestFixture
+    {
+        private MZSpectrum _mzSpectrumA;
+
+        [SetUp]
+        public void Setup()
+        {
+            double[] mz = { 328.73795, 329.23935, 447.73849, 448.23987, 482.23792, 482.57089, 482.90393, 500.95358, 501.28732, 501.62131, 611.99377, 612.32806, 612.66187, 722.85217, 723.35345 };
+            double[] intensities = { 81007096.0, 28604418.0, 78353512.0, 39291696.0, 122781408.0, 94147520.0, 44238040.0, 71198680.0, 54184096.0, 21975364.0, 44514172.0, 43061628.0, 23599424.0, 56022696.0, 41019144.0 };
+
+            _mzSpectrumA = new MZSpectrum(mz, intensities);
+        }
+
+        [Test]
+        public void EmptySpectrumCountIsZero()
+        {
+            Assert.AreEqual(0, MZSpectrum.Empty.Count);
+        }
+
+        #region Properties
+
+        [Test]
+        public void SpectrumCount()
+        {
+            Assert.AreEqual(15, _mzSpectrumA.Count);
+        }
+
+        [Test]
+        public void SpectrumFirstMZ()
+        {
+            Assert.AreEqual(328.73795, _mzSpectrumA.FirstMZ);
+        }
+
+        [Test]
+        public void SpectrumLastMZ()
+        {
+            Assert.AreEqual(723.35345, _mzSpectrumA.LastMZ);
+        }
+
+        #endregion Properties
+
+        [Test]
+        public void SpectrumBasePeakIntensity()
+        {
+            double basePeakIntensity = _mzSpectrumA.GetBasePeakIntensity();
+
+            Assert.AreEqual(122781408.0, basePeakIntensity);
+        }
+
+        [Test]
+        public void SpectrumTIC()
+        {
+            double tic = _mzSpectrumA.GetTotalIonCurrent();
+
+            Assert.AreEqual(843998894.0, tic);
+        }
+
+        [Test]
+        public void SpectrumGetMasses()
+        {
+            double[] mz = { 328.73795, 329.23935, 447.73849, 448.23987, 482.23792, 482.57089, 482.90393, 500.95358, 501.28732, 501.62131, 611.99377, 612.32806, 612.66187, 722.85217, 723.35345 };
+            double[] masses = _mzSpectrumA.GetMasses();
+
+            Assert.AreEqual(mz, masses);
+        }
+
+        [Test]
+        public void SpectrumGetIntensities()
+        {
+            double[] intensities = { 81007096.0, 28604418.0, 78353512.0, 39291696.0, 122781408.0, 94147520.0, 44238040.0, 71198680.0, 54184096.0, 21975364.0, 44514172.0, 43061628.0, 23599424.0, 56022696.0, 41019144.0 };
+            double[] intensities2 = _mzSpectrumA.GetIntensities();
+
+            Assert.AreEqual(intensities, intensities2);
+        }
+
+        [Test]
+        public void SpectrumToArray()
+        {
+            double[,] data = _mzSpectrumA.ToArray();
+            double[,] realData =
+            {
+                {328.73795, 329.23935, 447.73849, 448.23987, 482.23792, 482.57089, 482.90393, 500.95358, 501.28732, 501.62131, 611.99377, 612.32806, 612.66187, 722.85217, 723.35345}
+                , {81007096.0, 28604418.0, 78353512.0, 39291696.0, 122781408.0, 94147520.0, 44238040.0, 71198680.0, 54184096.0, 21975364.0, 44514172.0, 43061628.0, 23599424.0, 56022696.0, 41019144.0}
+            };
+
+            Assert.AreEqual(data, realData);
+        }
+
+        [Test]
+        public void SpectrumGetIntensityFirst()
+        {
+            double intensity = _mzSpectrumA.GetIntensity(0);
+
+            Assert.AreEqual(81007096.0, intensity);
+        }
+
+        [Test]
+        public void SpectrumGetIntensityRandom()
+        {
+            double intensity = _mzSpectrumA.GetIntensity(6);
+
+            Assert.AreEqual(44238040.0, intensity);
+        }
+
+        [Test]
+        public void SpectrumGetMassFirst()
+        {
+            double intensity = _mzSpectrumA.GetMass(0);
+
+            Assert.AreEqual(328.73795, intensity);
+        }
+
+        [Test]
+        public void SpectrumGetMassRandom()
+        {
+            double intensity = _mzSpectrumA.GetMass(6);
+
+            Assert.AreEqual(482.90393, intensity);
+        }
+
+        #region Contains Peak
+
+        [Test]
+        public void SpectrumContainsPeak()
+        {
+            Assert.IsTrue(_mzSpectrumA.ContainsPeak());
+        }
+
+        [Test]
+        public void SpectrumContainsPeakInRange()
+        {
+            Assert.IsTrue(_mzSpectrumA.ContainsPeak(448.23987 - 0.001, 448.23987 + 0.001));
+        }
+
+        [Test]
+        public void SpectrumContainsPeakInRangeEnd()
+        {
+            Assert.IsTrue(_mzSpectrumA.ContainsPeak(448.23987 - 0.001, 448.23987));
+        }
+
+        [Test]
+        public void SpectrumContainsPeakInRangeStart()
+        {
+            Assert.IsTrue(_mzSpectrumA.ContainsPeak(448.23987, 448.23987 + 0.001));
+        }
+
+        [Test]
+        public void SpectrumContainsPeakInRangeStartEnd()
+        {
+            Assert.IsTrue(_mzSpectrumA.ContainsPeak(448.23987, 448.23987));
+        }
+
+        [Test]
+        public void SpectrumContainsPeakInRangeBackwards()
+        {
+            Assert.IsFalse(_mzSpectrumA.ContainsPeak(448.23987 + 0.001, 448.23987 - 0.001));
+        }
+
+        [Test]
+        public void SpectrumDoesntContainPeakInRange()
+        {
+            Assert.IsFalse(_mzSpectrumA.ContainsPeak(603.4243 - 0.001, 603.4243 + 0.001));
+        }
+
+        #endregion Contains Peak
+
+        [Test]
+        public void SpectrumMassRange()
+        {
+            MzRange range = new MzRange(328.73795, 723.35345);
+
+            Assert.AreEqual(range, _mzSpectrumA.GetMzRange());
+        }
+
+        [Test]
+        public void SpectrumFilterEmpty()
+        {
+            var filteredMzSpectrum = _mzSpectrumA.FilterByIntensity(0, 50);
+
+            Assert.AreEqual(MZSpectrum.Empty, filteredMzSpectrum);
+        }
+
+        [Test]
+        public void SpectrumFilterCount()
+        {
+            var filteredMzSpectrum = _mzSpectrumA.FilterByIntensity(28604417, 28604419);
+
+            Assert.AreEqual(1, filteredMzSpectrum.Count);
+        }
+    }
+}
